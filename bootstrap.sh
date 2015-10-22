d77c3ce3ca07a91971473c04063cf906636ecbac
Meng Ding <dingmeng@gmail.com>
2015-10-22 22:32:52 +0000

    entrypoint script can monitor process status

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,6 +19,10 @@ init_config() {
   sed s/@HOSTNAME@/${CONF_RESOURCEMANAGER}/ ${OLD_YARN_SITE} > ${YARN_SITE}
 }
 
+log() {
+  echo $(date) $@
+}
+
 # Clean pid files
 rm -rf /tmp/hadoop*.pid
 
@@ -28,15 +32,19 @@ init_config
 # Startup daemons
 case $1 in
   namenode)
+    JPS_CLASS=NameNode
     ${HADOOP_PREFIX}/bin/hdfs --daemon start namenode 
   ;;
   datanode)
+    JPS_CLASS=DataNode
     ${HADOOP_PREFIX}/bin/hdfs --daemon start datanode
   ;;
   resourcemanager)
+    JPS_CLASS=ResourceManager
     ${HADOOP_PREFIX}/bin/yarn --daemon start resourcemanager 
   ;;   
   nodemanager)
+    JPS_CLASS=NodeManager
     ${HADOOP_PREFIX}/bin/yarn --daemon start nodemanager 
   ;;
   *)
@@ -44,4 +52,11 @@ case $1 in
   ;;  
 esac
 
-while true; do sleep 1000; done
+while true; do
+  if jps | grep $JPS_CLASS; then
+    sleep 15
+  else
+    log "$JPS_CLASS has exited unexpectedly"
+    exit 1
+  fi
+done
