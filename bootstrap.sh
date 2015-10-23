1bf1e0cc7b664f8e6f06db39a3ed279b731d199b
Meng Ding <dingmeng@gmail.com>
2015-10-23 15:11:53 +0000

    trap signals for the entrypoint script

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,37 +23,64 @@ log() {
   echo $(date) $@
 }
 
-# Clean pid files
+die() {
+  log "Stopping $DAEMON ..."
+  if do_action "stop"; then
+    log "Stopped $DAEMON"
+  fi
+  exit 1
+}
+
+do_action() {
+  action=$1
+  case $DAEMON in
+    namenode)
+      JPS_CLASS=NameNode
+      ${HADOOP_PREFIX}/bin/hdfs --daemon $1 namenode
+    ;;
+    datanode)
+      JPS_CLASS=DataNode
+      ${HADOOP_PREFIX}/bin/hdfs --daemon $1 datanode
+    ;;
+    resourcemanager)
+      JPS_CLASS=ResourceManager
+      ${HADOOP_PREFIX}/bin/yarn --daemon $1 resourcemanager
+    ;;
+    nodemanager)
+      JPS_CLASS=NodeManager
+      ${HADOOP_PREFIX}/bin/yarn --daemon $1 nodemanager
+    ;;
+    *)
+      log "Unrecognized daemon $DAEMON"
+      return 1
+    ;;
+  esac
+  return $?
+}
+
+DAEMON=$1
+
+# Clean pid files in case hadoop daemon is not gracefully shut down
 rm -rf /tmp/hadoop*.pid
+
+# Trap signal
+trap die SIGHUP SIGINT SIGTERM
 
 # Initialize configuration files if needed
 init_config
 
-# Startup daemons
-case $1 in
-  namenode)
-    JPS_CLASS=NameNode
-    ${HADOOP_PREFIX}/bin/hdfs --daemon start namenode 
-  ;;
-  datanode)
-    JPS_CLASS=DataNode
-    ${HADOOP_PREFIX}/bin/hdfs --daemon start datanode
-  ;;
-  resourcemanager)
-    JPS_CLASS=ResourceManager
-    ${HADOOP_PREFIX}/bin/yarn --daemon start resourcemanager 
-  ;;   
-  nodemanager)
-    JPS_CLASS=NodeManager
-    ${HADOOP_PREFIX}/bin/yarn --daemon start nodemanager 
-  ;;
-  *)
-  /bin/bash
-  ;;  
-esac
+log "Starting $DAEMON ..."
 
+# Start daemon
+if ! do_action "start"; then
+  exit 1
+fi
+
+log "Started $DAEMON"
+
+# Minitor daemon liveliness
 while true; do
-  if jps | grep $JPS_CLASS; then
+  if jps | grep $JPS_CLASS > /dev/null; then
     sleep 15
   else
     log "$JPS_CLASS has exited unexpectedly"
